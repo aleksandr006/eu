7bfd85932588e3dd796fa76be001c0eb37d11229
opilane <opilane@HP-CZC6297BBB>
2021-12-09 09:57:19 +0200

    xx

--- Eurigit.py
+++ Eurigit.py
@@ -38,3 +38,19 @@ for country in Countries:
             dictionary = {input("Sisesta riik"):input("Sisesta pealinn")}
         else:
             print("viga")
+sõnastik = {}
+riigid=[]
+linnad=[]
+file=open("riigid_pealinnad.txt","r")
+for line in file:
+    k,v=line.strip().split("-")
+    sonastik[k.strip()]=v.strip()
+    riigid.append(k)
+    linnad.append(sonastik)[k.strip8()])
+    file.close()
+    print(sonastik)
+    print("Riigid:")
+    print(riigid)
+    print("Pealinnad:")
+    print(linnad)
+    a=input()
