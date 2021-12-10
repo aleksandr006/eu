151e71583613ad62b36d33f7669f240574cfc563
opilane <opilane@HP-CZC6297B98>
2021-12-10 12:55:30 +0200

    h

--- Eurigit.py
+++ Eurigit.py
@@ -1,25 +1,25 @@
 from module1 import*
-riik_dict={"Austria": "Viin",
+riik_dict={"Austria": "Viin",# Loo tühi sõna Capital
+           "Estonia":"Tallinn",
                  "Belgia": "Brüssel",
                 "Saksamaa":" Berliin",
                  "Iirimaa": "Dublin",
-                 "Liechtenstein": "Vaduz",
+                 "Liechtenstein": "Vaduz",# Täitke see mitu tähendust
                 "Holland": "Amsterdam",
                  "Prantsusmaa":"Pariis",
                  "Bulgaaria": "Sofia",
-                 "Польша": "Варшава",
-                 "Чехия": "Прага",
-                 "Албания": "Тирана",
-                 "Босния и Герцеговина": "Сараево",
-                 "Северная Македония": "Скопье",
-                 "Сербия": "Белград"}
+                 "Poola":" Varssavi",
+                 "Tšehhi" : "Praha",
+                 "Albaania": "Tirana",
+                 "Bosnia ja Hertsegoviina": "Sarajevo",
+                 "Serbia ":" Belgrad"}
 print(riik_dict )
 while True:
-    print("Привет! Пройдёмся по странам и их столицам!")
-    print("1 - bUurige välja riigi pealinn või vastupidi,\n2 - Lisage uus riik ja pealinn,")
+    print("kontrollige teadmisi tähtedest ja nende pealinnadest")
+    print("1 - leida riik selle pealinna järgi ja vastupidi,\n2 - Lisage uus riik ja pealinn,")
     menu=input()
     if menu=="1":
-        v=input("Будем искать страну по стoлице(1) или стoлицу по стране(2)? ")
+        v=input("Otsime riiki pealinna järgi(1) või pealinn riigiti(2)? ")
         riik(riik_dict,v)
     elif menu=="2":
         new_key_value(riik_dict)
--- module1.py
+++ module1.py
@@ -3,24 +3,24 @@ def riik(d:dict, v:int):
     values_list=(list(d.values()))
     if v=="1":
         mas=[]
-        capital=(input("Введи столицу: ")).capitalize()
+        capital=(input("Sisestage pealinn: ")).capitalize()
         capital.title()
         for i in values_list:
             if i==capital:
                 for i in range(len(values_list)):
                     if values_list[i]==capital:
                         mas.append(int(i))
-                        print("Страна -", keys_list[i],"<-->", "Cтолица -", values_list[i])
+                        print("riik -", keys_list[i],"<-->", "peallin -", values_list[i])
     else:
-        riik=(input("Введите страну: ")).capitalize()
-        a=d.get(country)
-        print("Страна -", country ,"<-->", "Cтолица -", a)
+        riik=(input("Sisestage  riik: ")).capitalize()
+        a=d.get(riik)
+        print("riik -", riik ,"<-->", " -", a)
     return
 
 def new_key_value(d:dict):
     new={}
-    country=(input("Введите страну: ")).capitalize()
-    capital=(input("Введите страну: ")).capitalize()
+    riik=(input("Sisestage riik: ")).capitalize()
+    capital=(input("Sisestage peallin: ")).capitalize()
     new={riik:capital}
 
-    return d,new
+    return d,new                                                                                                                                                                                                        
